650162e188bc2cbf3ebc096a8ff7aca92a943829
devofficer <myeyesontech@gmail.com>
2022-11-10 10:25:12 -0800

    [feat] language button

--- src/assets/script/events.js
+++ src/assets/script/events.js
@@ -11,3 +11,29 @@ mobileLinks.forEach(link => {
     mobileMenu.classList.toggle('hidden');
   });
 });
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const code = urlParams.get('lang');
+const lang = code ? code.toUpperCase() : 'ES';
+
+function switchLang(lang) {
+  const elements = document.querySelectorAll(`[data-lang]`);
+  elements.forEach(element => {
+    const attr = element.getAttribute('data-lang');
+    if (attr === lang)
+      element.classList.remove('hidden');
+    else
+      element.classList.add('hidden');
+  });
+}
+switchLang(lang);
+
+const langBtns = document.querySelectorAll('.language a');
+langBtns.forEach(element => {
+  const lang = element.getAttribute('data-target');
+  element.addEventListener('click', () => {
+    switchLang(lang);
+  });
+});
+
