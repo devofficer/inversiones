3960e6533326a4686fbc84d377a1c2164ab63215
devofficer <myeyesontech@gmail.com>
2022-11-13 15:04:44 -0800

    [feat] update margin

--- src/assets/script/events.js
+++ src/assets/script/events.js
@@ -73,4 +73,4 @@ function animate(obj, initVal, lastVal, duration) {
 
 const counter = document.querySelector("#counter");
 console.log(counter);
-animate(counter, 0, 300, 20000);
+animate(counter, 0, 300, 5000);
