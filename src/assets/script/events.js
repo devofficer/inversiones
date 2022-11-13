a9c1ba49b9091d2f1b570b824d7f43976941ef7f
devofficer <myeyesontech@gmail.com>
2022-11-13 12:38:02 -0800

    [feat] counter

--- src/assets/script/events.js
+++ src/assets/script/events.js
@@ -1,3 +1,5 @@
+console.log("start");
+
 const toggleBtn = document.querySelector('button.menu-button');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileLinks = document.querySelectorAll('.mobile-menu li');
@@ -37,3 +39,57 @@ langBtns.forEach(element => {
   });
 });
 
+
+function sendEmail() {
+  Email.send({
+      Host : "smtp.mailtrap.io",
+      Username : "<Mailtrap username>",
+      Password : "<Mailtrap password>",
+      To : 'recipient@example.com',
+      From : "sender@example.com",
+      Subject : "Test email",
+      Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+  }).then(
+    message => alert(message)
+  );
+}
+
+const submitBtn = document.querySelector('#submit_contact');
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+
+});
+
+function animate(obj, initVal, lastVal, duration) {
+  let startTime = null;
+
+  //get the current timestamp and assign it to the currentTime variable
+  let currentTime = Date.now();
+
+  //pass the current timestamp to the step function
+  const step = (currentTime ) => {
+    //if the start time is null, assign the current time to startTime
+    if (!startTime) {
+      startTime = currentTime ;
+    }
+
+    //calculate the value to be used in calculating the number to be displayed
+    const progress = Math.min((currentTime - startTime) / duration, 1);
+
+    //calculate what to be displayed using the value gotten above
+    obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal).toLocaleString('en-US', {style:"currency", currency:"EUR", minimumFractionDigits: 3});
+
+    //checking to make sure the counter does not exceed the last value (lastVal)
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    } else {
+      window.cancelAnimationFrame(window.requestAnimationFrame(step));
+    }
+  };
+
+//start animating
+  window.requestAnimationFrame(step);
+}
+
+const counter = document.querySelector("#counter");
+animate(counter, 0, 300, 4000);
