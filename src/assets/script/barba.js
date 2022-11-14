6f1ba0a63be661d0014a6066dd2f9ee8c4ee9f8a
devofficer <myeyesontech@gmail.com>
2022-11-14 04:40:20 -0800

    [feat] animation for whole content

--- src/assets/script/barba.js
+++ src/assets/script/barba.js
@@ -1,83 +1,62 @@
 barba.init();
 
-function appearContentY(element) {
-    const posDelta = -100;
-    const duration = 0.5;
-
-    gsap.fromTo(element, { x: 0, y: posDelta, autoAlpha: 0 }, {
-      duration: duration, 
-      x: 0,
-      y: 0, 
+function appearContent(element) {
+    gsap.fromTo(element, { scaleX: 0.9, scaleY: 0.9, autoAlpha: 0 }, {
+      duration: 2, 
+      scaleX: 1,
+      scaleY: 1,
       autoAlpha: 1, 
       ease: "expo", 
       overwrite: "auto"
     });
 }
 
-function appearContentX(element, reverse, durDelta) {
-    const flag = reverse ? -1 : 1;
-    const posDelta = (500 + durDelta * 300) * flag;
-    const duration = 2 + 0.5 * durDelta;
+function appearFooter(element) {
+  gsap.fromTo(element, { y: 200, autoAlpha: 0.4 }, {
+    duration: 2, 
+    y: 0,
+    autoAlpha: 1, 
+    ease: "expo", 
+    overwrite: "auto"
+  });
+}
 
-    gsap.fromTo(element, { x: posDelta, y: 0, autoAlpha: 0 }, {
-      duration: duration, 
-      x: 0,
-      y: 0, 
-      autoAlpha: 1, 
-      ease: "expo", 
-      overwrite: "auto"
+document.addEventListener("DOMContentLoaded", function() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray(".section-img-container").forEach(function(element, index) {
+      ScrollTrigger.create({
+          trigger: element,
+          onEnter: function() { appearContent(element, true, index) }, 
+          onEnterBack: function() { appearContent(element)},
+          // onLeave: function() { hide(elem) }
+      });
+  });
+
+  gsap.utils.toArray(".sub-item").forEach(function(element, index) {
+    ScrollTrigger.create({
+        trigger: element,
+        onEnter: function() { appearContent(element) }, 
+        onEnterBack: function() { appearContent(element) },
+        // onLeave: function() { hideContent(element) }
     });
-}
-  
-function hide(elem) {
-    gsap.set(elem, { autoAlpha: 0 });
-}
-  
-// document.addEventListener("DOMContentLoaded", function() {
-//     gsap.registerPlugin(ScrollTrigger);
+  });
 
-//     gsap.utils.toArray(".section-img-container").forEach(function(element, index) {
-//         ScrollTrigger.create({
-//             trigger: element,
-//             onEnter: function() { appearContentX(element, true, index) }, 
-//             // onEnterBack: function() {},
-//             // onLeave: function() { hide(elem) }
-//         });
-//     });
+  gsap.utils.toArray("section").forEach(function(element, index) {
+    ScrollTrigger.create({
+        trigger: element,
+        onEnter: function() { appearContent(element) }, 
+        onEnterBack: function() { appearContent(element)},
+        // onLeave: function() { hideContent(element) }
+    });
+  });
 
-//     gsap.utils.toArray(".section-content > *").forEach(function(element, index) {
-//         ScrollTrigger.create({
-//             trigger: element,
-//             onEnter: function() { appearContentX(element, false, index) }, 
-//             // onEnterBack: function() {},
-//             // onLeave: function() { hide(elem) }
-//         });
-//     });
-
-//     gsap.utils.toArray(".section-img-container-reverse").forEach(function(element, index) {
-//         ScrollTrigger.create({
-//             trigger: element,
-//             onEnter: function() { appearContentX(element, false, index) }, 
-//             // onEnterBack: function() {},
-//             // onLeave: function() { hide(elem) }
-//         });
-//     });
-
-//     gsap.utils.toArray(".section-content-reverse > *").forEach(function(element, index) {
-//         ScrollTrigger.create({
-//             trigger: element,
-//             onEnter: function() { appearContentX(element, true, index) }, 
-//             // onEnterBack: function() {},
-//             // onLeave: function() { hide(elem) }
-//         });
-//     });
-
-//     gsap.utils.toArray("nav").forEach(function(element, index) {
-//         ScrollTrigger.create({
-//             trigger: element,
-//             onEnter: function() { appearContentY(element) }, 
-//             // onEnterBack: function() {},
-//             // onLeave: function() { hide(elem) }
-//         });
-//     });
-// });
+  gsap.utils.toArray(".footer").forEach(function(element, index) {
+    ScrollTrigger.create({
+        trigger: element,
+        onEnter: function() { appearFooter(element) }, 
+        onEnterBack: function() { appearFooter(element)},
+        // onLeave: function() { hideContent(element) }
+    });
+  });
+});
